cb4bd0bf9fde341f7025307c477a98c532b6c50b
Moeefa <moeefa@protonmail.com>
2022-02-04 23:21:25 -0400

    Update clear.js

--- commands/mod/clear.js
+++ commands/mod/clear.js
@@ -1,22 +1,23 @@
-const { RichEmbed } = require('discord.js');
-module.exports.run = async function(client, message, args) {
-  if (!args[0]) return client.embed(message.channel, `${message.author}, args[0] needed.`);
-  if (isNaN(args[0])) return client.embed(message.channel, '<@' + message.author.id + '> ' + 'Detected args[0] as isNaN (is Not a Number)');
-  if (args[0] > 100) return client.embed(message.channel, '<@' + message.author.id + '> ' + 'args[0] is higher than Discord API supports.');
-  if (args[0] < 1) return client.embed(message.channel, '<@' + message.author.id + '> ' + 'args[0] is lower than Discord API supports.');
+const { MessageEmbed } = require('discord.js');
+exports.run = async function(bot, msg, args) {
+  if (!args[0]) return msg.reply(`args[0] needed.`);
+  if (isNaN(args[0])) return msg.reply('Detected args[0] as isNaN (is Not a Number)');
+  if (args[0] > 100) return msg.reply('args[0] is higher than Discord API supports.');
+  if (args[0] < 1) return msg.reply('args[0] is lower than Discord API supports.');
   
-  await message.delete()
+  await msg.delete()
   
-  message.channel.bulkDelete(args[0], true).then(async messages => {
-    if (messages.size === 0) {
-      client.embed(message.channel, 'You can only bulk delete messages that are under 14 days old.');
+  msg.channel.bulkDelete(args[0], true).then(async messages => {
+    if (messages.size == 0) {
+      msg.reply('You can only bulk delete messages that are under 14 days old.');
       return;
-    }
-    client.embed(message.channel, `Deleted \`\`${messages.size}/${args[0]}\`\` message(s)!\nCommand requisited by: \`\`${message.author.tag}\`\`.`);
+    };
+
+    msg.reply(`Deleted ${messages.size}/${args[0]} message(s)!\nCommand requisited by: ${message.author.tag}.`);
   });
 };
 
-module.exports.conf = {
+exports.conf = {
   enabled: true,
   guildOnly: true,
   aliases: [],
@@ -25,7 +26,7 @@ module.exports.conf = {
   permLevel: 'User',
 };
 
-module.exports.help = {
+exports.help = {
   name: 'clear',
   category: 'Moderation',
   description: 'Delete messages in the chat.',
