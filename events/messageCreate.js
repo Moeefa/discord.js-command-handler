a943b2fa7c0e07d206add9eb85540c76b1c80585
Moeefa <moeefa@protonmail.com>
2022-03-04 15:40:13 -0400

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -8,34 +8,34 @@ module.exports = async function(bot, msg) {
   const cmd = bot.commands.get(command) || bot.commands.get(bot.aliases.get(command));
   
   if (!cmd) return;
-  if (cmd.help.category == bot.config.devFolder && msg.author.id !== bot.config.ownerID) return msg.reply("You're not my owner.")
+  if (cmd.category == bot.config.devFolder && msg.author.id !== bot.config.ownerID) return msg.reply("You're not my owner.")
   if (cmd.conf && cmd.conf.enabled == false) return;
   if (!msg.guild && cmd.conf && cmd.conf.guildOnly == true) return msg.reply("You can only use this command in guilds!");
   
   // Check if the bot has permission to run the command.
-  if (cmd.conf && cmd.conf.botPermNeeded && cmd.conf.botPermNeeded.length >= 1) {
-    for (var i in cmd.conf.botPermNeeded) {
-      if (typeof cmd.conf.botPermNeeded[i] !== "string") continue;
-      if (!msg.guild.me.permissions.has(cmd.conf.botPermNeeded[i])) {
-        msg.reply(`I don't have the ${cmd.conf.botPermNeeded[i]} permission to run this command.`);
+  if (cmd.conf && cmd.botPerm && cmd.botPerm.length >= 1) {
+    for (var i in cmd.botPerm) {
+      if (typeof cmd.botPerm[i] !== "string") continue;
+      if (!msg.guild.me.permissions.has(cmd.botPerm[i])) {
+        msg.reply(`I don't have the ${cmd.botPerm[i]} permission to run this command.`);
         return;
       };
     };
   };
 
   // Check if member that run the command has permission to it.
-  if (cmd.conf && cmd.conf.memberPermNeeded && cmd.conf.memberPermNeeded.length >= 1) {
-    for (var i in cmd.conf.memberPermNeeded) {
-      if (typeof cmd.conf.memberPermNeeded[i] !== "string") continue;
-      if (!msg.member.permissions.has(cmd.conf.memberPermNeeded[i])) {
-        msg.reply(`You don't have the ${cmd.conf.memberPermNeeded[i]} permission to run this command.`);
+  if (cmd.conf && cmd.memberPerm && cmd.memberPerm.length >= 1) {
+    for (var i in cmd.memberPerm) {
+      if (typeof cmd.memberPerm[i] !== "string") continue;
+      if (!msg.member.permissions.has(cmd.memberPerm[i])) {
+        msg.reply(`You don't have the ${cmd.memberPerm[i]} permission to run this command.`);
         return;
       };
     };
   };
                                  
   try {
-    cmd.run(bot, msg, args, embed)
+    cmd.run(bot, msg, args)
   } catch (err) {
     throw err;
   };
