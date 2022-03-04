c8d9d5022ed27d8f228b64c20f66011d5bb370b8
Moeefa <moeefa@protonmail.com>
2022-03-04 15:43:19 -0400

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,28 +1,14 @@
 const { MessageEmbed } = require('discord.js');
 
-exports.run = async (bot, msg, args) => {
-  const embed = new MessageEmbed()
-  .setDescription('Ping?')
-  .setColor(bot.config.primaryColor);
-  const pingMsg = await msg.channel.send({ embeds: [embed] });
+module.exports = {
+  async run(bot, msg, args) {
+    const embed = new MessageEmbed()
+    .setDescription('Ping?')
+    .setColor(bot.config.primaryColor);
+    const pingMsg = await msg.channel.send({ embeds: [embed] });
 
-  embed.setColor(bot.config.primaryColor);
-  embed.setDescription(`Pong! 🏓 My ping is ${msg.createdTimestamp - message.createdTimestamp}ms. API's ping is ${Math.round(client.ping)}ms`);
-  pingMsg.edit(embed);
-};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  botPermNeeded: [],
-  memberPermNeeded: [],
-  permLevel: 'User'
-};
-
-exports.help = {
-  name: 'ping',
-  category: 'Informations',
-  description: 'Sends bot ping.',
-  usage: 'ping'
+    embed.setColor(bot.config.primaryColor);
+    embed.setDescription(`Pong! 🏓 My ping is ${msg.createdTimestamp - message.createdTimestamp}ms. API's ping is ${Math.round(client.ping)}ms`);
+    pingMsg.edit(embed);
+  }
 };
