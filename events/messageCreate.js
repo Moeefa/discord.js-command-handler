688a9d05f0ec58b4b19d55d8cb6864e5fb86932e
Moeefa <moeefa@protonmail.com>
2022-03-04 16:04:28 -0400

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -9,22 +9,22 @@ module.exports = async function(bot, msg) {
   
   if (!cmd) return;
   if (cmd.category == bot.config.devFolder && msg.author.id !== bot.config.ownerID) return msg.reply("You're not my owner.")
-  if (cmd.conf && cmd.conf.enabled == false) return;
+  if (cmd.enabled == false) return;
   if (!msg.guild && cmd.conf && cmd.conf.guildOnly == true) return msg.reply("You can only use this command in guilds!");
   
   // Check if the bot has permission to run the command.
-  if (cmd.conf && cmd.botPerm && cmd.botPerm.length >= 1) {
-    for (var i in cmd.botPerm) {
-      if (typeof cmd.botPerm[i] !== "string") continue;
-      if (!msg.guild.me.permissions.has(cmd.botPerm[i])) {
-        msg.reply(`I don't have the ${cmd.botPerm[i]} permission to run this command.`);
+  if (cmd.clientPerm && cmd.clientPerm.length >= 1) {
+    for (var i in cmd.clientPerm) {
+      if (typeof cmd.clientPerm[i] !== "string") continue;
+      if (!msg.guild.me.permissions.has(cmd.clientPerm[i])) {
+        msg.reply(`I don't have the ${cmd.clientPerm[i]} permission to run this command.`);
         return;
       };
     };
   };
 
   // Check if member that run the command has permission to it.
-  if (cmd.conf && cmd.memberPerm && cmd.memberPerm.length >= 1) {
+  if (cmd.memberPerm && cmd.memberPerm.length >= 1) {
     for (var i in cmd.memberPerm) {
       if (typeof cmd.memberPerm[i] !== "string") continue;
       if (!msg.member.permissions.has(cmd.memberPerm[i])) {
