3a40b6ade9bef98f47b2b478c3a623e867580e82
Moeefa <moeefa@protonmail.com>
2022-03-04 16:00:14 -0400

    Create _base.js

---
+++ commands/_base.js
@@ -0,0 +1,12 @@
+module.exports = {
+  enabled: Boolean, // Default: true.
+  guildOnly: Boolean, // Default: true.
+  name: String, // Default: file name.
+  aliases: Array, // Doesn't have default value.
+  description: String, // Doesn't have default value.
+  usage: String // Doesn't have default value.
+  category: String, // Default: folder name.
+  clientPerm: Array, // Doesn't have default value.
+  memberPerm: Array, // Doesn't have default value.
+  run: Function // Doesn't have default value and it's required.
+}
