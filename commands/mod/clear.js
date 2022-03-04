92b4de3f5b8b668ff9effc11325fdcde42f708fc
Moeefa <moeefa@protonmail.com>
2022-03-04 15:47:04 -0400

    Update clear.js

--- commands/mod/clear.js
+++ commands/mod/clear.js
@@ -1,34 +1,23 @@
 const { MessageEmbed } = require('discord.js');
-exports.run = async function(bot, msg, args) {
-  if (!args[0]) return msg.reply(`args[0] needed.`);
-  if (isNaN(args[0])) return msg.reply('Detected args[0] as isNaN (is Not a Number)');
-  if (args[0] > 100) return msg.reply('args[0] is higher than Discord API supports.');
-  if (args[0] < 1) return msg.reply('args[0] is lower than Discord API supports.');
-  
-  await msg.delete()
-  
-  msg.channel.bulkDelete(args[0], true).then(async messages => {
-    if (messages.size == 0) {
-      msg.reply('You can only bulk delete messages that are under 14 days old.');
-      return;
-    };
-
-    msg.reply(`Deleted ${messages.size}/${args[0]} message(s)!\nCommand requisited by: ${message.author.tag}.`);
-  });
-};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: [],
-  botPermNeeded: ["MANAGE_MESSAGES"],
-  memberPermNeeded: ["MANAGE_MESSAGES"],
-  permLevel: 'User',
-};
-
-exports.help = {
+module.exports = {
   name: 'clear',
   category: 'Moderation',
   description: 'Delete messages in the chat.',
-  usage: 'clear [Amount]'
+  usage: 'clear [Amount]',
+  clientPerm: ["MANAGE_MESSAGES"],
+  memberPerm: ["MANAGE_MESSAGES"],
+  async run(bot, msg, args) {
+    if (!args[0]) return msg.reply(`args[0] needed.`);
+    if (isNaN(args[0])) return msg.reply('Detected args[0] as isNaN (is Not a Number)');
+    if (args[0] > 100) return msg.reply('args[0] is higher than Discord API supports.');
+    if (args[0] < 1) return msg.reply('args[0] is lower than Discord API supports.');
+  
+    await msg.delete()
+  
+    msg.channel.bulkDelete(args[0], true).then(async messages => {
+      if (messages.size == 0) return msg.reply('You can only bulk delete messages that are under 14 days old.');
+
+      msg.reply(`Deleted ${messages.size}/${args[0]} message(s)!\nCommand requisited by: ${message.author.tag}.`);
+    });
+  }
 };
