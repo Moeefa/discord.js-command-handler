670ad62deda51ed713668c2df39c26b784741786
Moeefa <32604322+Moeefa@users.noreply.github.com>
2022-08-04 08:13:08 -0400

    Update _base.js

--- commands/_base.js
+++ commands/_base.js
@@ -7,23 +7,18 @@
  * @property {string} name - Command name.
  * @property {string} description - Command description, can be an i18n string.
  * @property {string} category - Command category.
- * @property {array} aliases - Aliases of the command.
- * @property {string} usage - Explains how to use the command, can be an i18n string.
- * @property {array} botPermNeeded - Permissions flags that the bot need in the guild before the command is ran.
- * @property {array} memberPermNeeded - Permissions flags that the member need in the guild before the command is ran.
- * @property {boolean} enabled - Wether command is enabled.
- * @property {boolean} guildOnly - Wether if command is exclusive to guilds.
- * @property {function} run - The function that runs the command.
- * @returns {Object} Configurations of the command.
+ * @property {array} options - Explains how to use the command, can be an i18n string.
+ * @property {string||array} defaultMemberPermissions - Permissions flags bits that the member need in the guild before the command is ran.
+ * @property {boolean} dmPermission - Wether if command is exclusive to guilds.
+ * @property {function} execute - The function that runs the command.
+ * @returns {Object} Configurations and execute function of the command.
  */ 
-module.exports = {
+export default {
   /**
    * @function
-   * @name run
+   * @name execute
    * @description Required to run a command.
-   * @param {Object} client - The client of your Discord bot.
-   * @param {Object} message - The message released from messageCreate event.
-   * @param {array} args - The message content splited.
+   * @param {Object} interaction - The interaction received from user.
    */
-  run(client, message, args) {}
+  execute(interaction) {}
 };
